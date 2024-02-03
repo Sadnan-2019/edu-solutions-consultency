1796eb4216b831fe4c38cae0e6897b474ef50f10
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2024-02-03 22:58:35 +0600

    servicesr

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,55 +1,47 @@
 import React from "react";
-import uk from "../../assets/united-kingdom.png"
-import australia from "../../assets/australia.png"
-import usa from "../../assets/united-states-of-america.png"
-import germany from "../../assets/german.png"
-import japan from "../../assets/japan.png"
-import switzerland from "../../assets/switzerland.png"
-import canada from "../../assets/canada.png"
+import uk from "../../assets/united-kingdom.png";
+import australia from "../../assets/australia.png";
+import usa from "../../assets/united-states-of-america.png";
+import germany from "../../assets/german.png";
+import japan from "../../assets/japan.png";
+import switzerland from "../../assets/switzerland.png";
+import canada from "../../assets/canada.png";
+import china from "../../assets/china.png";
 
 const Country = () => {
   return (
     <div className="bg-gradient-to-r from-[#3F1825]  via-[#302530] to-[#441B26]">
       <body class=" ">
         <div class="text-gray-300 container mx-auto p-8 overflow-hidden md:rounded-lg md:p-10 lg:p-12">
-          <p class="font-sans text-4xl font-bold text-gray-200 max-w-5xl lg:text-7xl lg:pr-24 md:text-6xl">
+          <p class="font-sans text-4xl font-bold text-gray-200 max-w-5xl lg:text-5xl lg:pr-24 md:text-5xl">
             Choose Your Destination
           </p>
 
-          <div class="container relative z-40 mx-auto mt-12">
+          <div class="container relative z-40 mx-auto  py-12">
             <div class="flex flex-wrap justify-center mx-auto lg:w-full md:w-5/6 xl:shadow-small-blue">
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src={uk}
-                    class="block mx-auto w-20"
-                  />
+                  <img src={uk} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
-                   STUDY UK
+                    STUDY UK
                   </p>
                 </div>
               </a>
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src={australia}
-                    class="block mx-auto w-20"
-                  />
+                  <img src={australia} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
-                   STUDY AUSTRALIA
+                    STUDY AUSTRALIA
                   </p>
                 </div>
               </a>
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src={usa}
-                    class="block mx-auto w-20"
-                  />
+                  <img src={usa} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
                     STUDY USA
@@ -59,10 +51,7 @@ const Country = () => {
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src={germany}
-                    class="block mx-auto w-20"
-                  />
+                  <img src={germany} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
                     STUDY GERMANY
@@ -72,10 +61,7 @@ const Country = () => {
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src={japan}
-                    class="block mx-auto w-20"
-                  />
+                  <img src={japan} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
                     STUDY JAPAN
@@ -85,10 +71,7 @@ const Country = () => {
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src={switzerland}
-                    class="block mx-auto w-20"
-                  />
+                  <img src={switzerland} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
                     STUDY SWITZERLAND
@@ -98,27 +81,20 @@ const Country = () => {
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    alt=""
-                    src={canada}
-                    class="block mx-auto w-20"
-                  />
+                  <img alt="" src={canada} class="block mx-auto w-20" />
 
                   <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
-                  STUDY CANADA
+                    STUDY CANADA
                   </p>
                 </div>
               </a>
 
               <a href="#" class="block w-1/2 py-10 text-center border lg:w-1/4">
                 <div>
-                  <img
-                    src="https://redpixelthemes.com/assets/images/icon-health-green.svg"
-                    class="block mx-auto"
-                  />
+                  <img src={china} class="block mx-auto w-20" />
 
-                  <p class="pt-4 text-sm font-medium capitalize font-body text-green-900 lg:text-lg md:text-base md:pt-6">
-                  Other Visa Services
+                  <p class="pt-4 text-sm font-medium capitalize font-body text-white lg:text-lg md:text-base md:pt-6">
+                    STUDY CHINA
                   </p>
                 </div>
               </a>
@@ -129,17 +105,21 @@ const Country = () => {
 
           <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-2">
             <div class="flex flex-col justify-center">
-              <h2 class="text-4xl font-bold">Made for devs and designers</h2>
-              <div class="h-6"></div>
+              {/* <h2 class="text-4xl font-bold">Made for devs and designers</h2>
+              <div class="h-6 bg-white"></div> */}
               <p class="font-serif text-xl text-gray-100 md:pr-10">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
-                autem, a recusandae vero praesentium qui impedit doloremque
-                molestias necessitatibus.
+                The unwavering determination and contemporary market insights of
+                EDU Solutions team have effectively charted the course for
+                discovering fresh opportunities and achieving substantial
+                outcomes for students. Consequently, EDU Solutions has garnered
+                years of outstanding proficiency in steering students towards a
+                successful future through unparalleled educational consultancy
+                services.
               </p>
-              <div class="h-8"></div>
-              <div class="grid grid-cols-2 gap-4 pt-8 border-t border-gray-800"></div>
+              {/* <div class="h-8 bg-white"></div> */}
+              <div class="grid grid-cols-2 gap-4 pt-8 border-t border-white-800 mt-10"></div>
             </div>
-            <div class="-mr-20 p-7 md:rounded-l-full bg-gradient-to-b from-gray-900 to-black shadow-md rounded-xl flex items-center justify-center">
+            <div class="py-12   -mr-20  md:rounded-l-full bg-gradient-to-b from-gray-900 to-black shadow-md rounded-xl flex items-center justify-center  ">
               <p class="text-xl  text-justify text-white w-96  ">
                 Edusolutions stands out as one of the leading and premier
                 education consultancy firms in Bangladesh, specializing in
@@ -151,14 +131,14 @@ const Country = () => {
             </div>
           </div>
 
-          <p class="font-serif text-4xl">
+          {/* <p class="font-serif text-4xl">
             <span class="text-gray-400">If we work all together</span>
 
             <span class="text-gray-600">
               consectetur adipisicing elit. Consectetur atque molestiae omnis
               excepturi enim!
             </span>
-          </p>
+          </p> */}
 
           <div class="h-32 md:h-40"></div>
 
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -15,11 +15,11 @@ const Services = () => {
         <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div class="max-w-xl mx-auto text-center xl:max-w-2xl">
             <h2 class="text-3xl font-bold leading-tight text-gray-50 sm:text-4xl xl:text-5xl mb-6">
-              We are just getting started!
+              Our Services
             </h2>
-            <p class="mb-4">
-              We are creating a tool that helps you be more productive and
-              efficient when building websites and webapps
+            <p class="mb-4 text-xl">
+              Fulfilling your needs with the utmost precision, we ensure to
+              deliver the finest results possible
             </p>
           </div>
           <div class="grid max-w-4xl lg:max-w-6xl grid-cols-1 mx-auto mt-8 text-center gap-y-4 sm:gap-x-8 sm:grid-cols-2 lg:grid-cols-3 sm:mt-12 lg:mt-20 sm:text-left">
@@ -313,7 +313,7 @@ const Services = () => {
                 </div>
               </div>
             </div>
-             
+
             <div class="overflow-hidden bg-white shadow-md rounded-xl">
               <div class="p-9">
                 <svg
@@ -492,7 +492,7 @@ const Services = () => {
                   ></path>
                 </svg>
                 <h3 class="mt-6 text-2xl font-bold text-gray-900 sm:mt-10">
-               Interview Preparation
+                  Interview Preparation
                 </h3>
                 <p class="mt-6 text-base text-gray-600">
                   Attach your own custom domain to your published projects or
@@ -554,7 +554,7 @@ const Services = () => {
                   ></path>
                 </svg>
                 <h3 class="mt-6 text-2xl font-bold text-gray-900 sm:mt-10">
-               Scholarship
+                  Scholarship
                 </h3>
                 <p class="mt-6 text-base text-gray-600">
                   Attach your own custom domain to your published projects or
