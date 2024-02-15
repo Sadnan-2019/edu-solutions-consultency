3c4eff3532b8ef67cb3551dd3c7ea5a60b23df67
Sadnan-2019 <sadnan.wow@gmail.com>
2024-02-15 16:50:59 +0600

    FQQy

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Faq = () => {
-    return (
-        <div>
-            <section class="bg-gradient-to-r from-[#080C24] via-[#3342BF] to-[#080C24] via-[#00395D] to-[#080C24] text-gray-100   min-h-screen">
-  <div class="container flex flex-col justify-center p-4 mx-auto md:p-8">
+  return (
+    <div>
+      <section class="bg-gradient-to-r from-[#080C24] via-[#3342BF] to-[#080C24] via-[#00395D] to-[#080C24] text-gray-100   min-h-screen">
+        {/* <div class="container flex flex-col justify-center p-4 mx-auto md:p-8">
     <h2 class="mb-12 text-4xl font-bold leadi text-center sm:text-5xl">Frequently Asked Questions</h2>
     <div class="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
       <details>
@@ -52,11 +52,107 @@ const Faq = () => {
         </div>
       </details>
     </div>
-  </div>
-</section>
-
+  </div> */}
+        <h2
+          className="text-3xl text-center text-white  font-bold poppins-b sm:text-4xl py-5"
+        
+        >
+          Frequently Asked Questions
+        </h2>
+        <div className="text-black grid grid-cols-1 lg:grid-cols-1 gap-6 items-center justify-items-center py-8 mx-5">
+          <div className=" ">
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                I would like to get all services at home. How is it possible?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <li>Contact us and talk to our expert</li>
+                <li>Select service after baseline evaluation</li>
+                <li>Paying for the service</li>
+                <p className="mt-5">
+                  Our expert will suggest what is best for your specific needs:
+                  Recovery Program at home or Center based care.
+                </p>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                I am not sure which program fits my needs. What should I do?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <p className="mt-5">
+                  Talk to our expert on 01619848555 or visit your nearest HCAH
+                  Care Center
+                </p>
+                <p className="mt-5">
+                  They will understand your requirement and suggest the best
+                  course of action
+                </p>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                Are all your professionals medically trained?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <p className="mt-5 text-justify">
+                  We have certified,registered, and experienced professionals
+                  including doctors,nurses,physiotherapists,caregiver,medical
+                  assistant,medical caregiver.
+                </p>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="collapse collapse-plus  bg-base-100 rounded-box mb-5 "
+            >
+              <div className="collapse-title text-[18px] lg:text-[20px] font-medium  ">
+                What is the difference between recovery at the HCAH care center
+                and at the hospital?
+              </div>
+              <div className="collapse-content text-[15px] lg:text-[18px]">
+                <p className="mt-5 text-justify">
+                  The objective of treatment at the hospital is to save your
+                  life. You are discharged from the hospital once you are
+                  medically stable..
+                </p>
+                <p className="mt-5 text-justify">
+                  The objective of the HCAH care center is to rebuild the
+                  physical, functional, and cognitive abilities you've lost
+                  because of the disease and its the treatment so that you can
+                  return to normal life again.
+                </p>
+                <p className="mt-5 text-justify">
+                  When you are discharged from the HCAH care center, you are not
+                  just medically stable, you are physically, mentally, and
+                  emotionally healthy.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* <div>
+          <img
+            className="rounded"
+            src="https://www.tbsnews.net/sites/default/files/styles/big_2/public/images/2020/04/07/burn_hospital_salahuddin_ahmed_2.jpg?itok=-XDbK1lX&timestamp=1586250124"
+            alt=""
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+          />
+        </div> */}
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Faq;
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -229,6 +229,57 @@ const Review = () => {
                   </div>
                 </div>
               </div>
+              <div
+                // style="box-shadow:0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)"
+                class="mx-5  bg-gradient-to-r from-[#1AA855] via-[#051431] to-[#013125]    flex flex-col max-w-md align-center justify-between border border-solid border-gray-200 dark:border-dark    rounded-xl"
+              >
+                <div class="flex flex-col px-6 pt-8  space-y-5">
+                  <svg
+                    width="24"
+                    height="18"
+                    viewBox="0 0 24 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="text-white dark:text-gray-600 fill-current"
+                  >
+                    <path
+                      d="M24 7.3h-5.1L22.3.4H17l-3.4 6.9v10.3H24V7.3zM10.3 17.6V7.3H5L8.6.4H3.4L0 7.3v10.3h10.3z"
+                      fill="current"
+                    ></path>
+                  </svg>
+                  <p
+                    class=" text-lg body-medium m-0 text-white dark:text-dark-contrastText"
+                    // style="hyphens:auto"
+                  >
+                    These are high-quality courses. Trust me. I own around 10
+                    and the price is worth it for the content quality.{" "}
+                    <a href="https://twitter.com/EducativeInc?ref_src=twsrc%5Etfw">
+                      @EducativeInc
+                    </a>{" "}
+                    came at the right time in my career. I'm understanding
+                    topics better than with any book or online video tutorial
+                    I've done. Truly made for developers. Thanks{" "}
+                    {/* <a href="https://t.co/EeKruv5hxM">
+                      https://t.co/EeKruv5hxM
+                    </a> */}
+                  </p>
+                </div>
+
+                <div class="flex justify-between items-center px-5 py-5">
+                  <div>
+                    <p class="font-display text-base text-white">
+                      Sheryl Berge
+                    </p>
+                  </div>
+                  <div class="overflow-hidden rounded-full ">
+                    <img
+                      alt=""
+                      class="h-14 w-14 object-cover bg-transparent"
+                      src="https://randomuser.me/api/portraits/men/15.jpg"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </Marquee>
