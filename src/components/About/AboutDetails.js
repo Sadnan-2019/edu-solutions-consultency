6e136b0b02cb015d24a72f67ed084c876fa059f1
Sadnan-2019 <sadnan.wow@gmail.com>
2024-05-09 16:25:41 +0600

    uk

--- src/components/About/AboutDetails.js
+++ src/components/About/AboutDetails.js
@@ -6,7 +6,7 @@ const AboutDetails = () => {
       <section class="w-full mx-auto py-10 bg-gray-50 dark:bg-gray-900 dark:text-white">
         {/* <div class="w-fit pb-1 px-2 mx-4 rounded-md text-2xl font-semibold border-b-2 border-blue-600 dark:border-b-2 dark:border-yellow-600">Services</div> */}
 
-        <div class="xl:w-[80%] sm:w-[85%] xs:w-[90%] mx-auto flex md:flex-row xs:flex-col lg:gap-4 xs:gap-2 justify-center lg:items-stretch md:items-center mt-4">
+        <div class="xl:w-[80%] sm:w-[85%] xs:w-[90%] mx-auto sm:flex md:flex lg:flex xs:flex-col lg:gap-4 xs:gap-2 justify-center lg:items-stretch md:items-center mt-4">
           <div class="lg:w-[50%] xs:w-full">
             <img
               class="lg:rounded-t-lg sm:rounded-sm xs:rounded-sm"
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
-
+import partner1 from "../../assets/partner/p1.jpeg";
+import partner2 from "../../assets/partner/p2.jpg";
+import partner3 from "../../assets/partner/p3.png";
+import partner4 from "../../assets/partner/p4.png";
+import partner5 from "../../assets/partner/p5.jpg";
+import partner6 from "../../assets/partner/p6.jpg";
+import partner7 from "../../assets/partner/p7.png";
+import partner8 from "../../assets/partner/p8.jpg";
+import partner9 from "../../assets/partner/p9.png";
+import partner10 from "../../assets/partner/p10.png";
+// import partner11 from "../../assets/partner/p11.jpg";
+import partner12 from "../../assets/partner/p12.png";
+import partner13 from "../../assets/partner/p13.jpg";
+import partner14 from "../../assets/partner/p14.png";
+import partner15 from "../../assets/partner/p15.png";
+// import partner16 from "../../assets/partner/p16.jpg";
+import partner17 from "../../assets/partner/p17.png";
+import partner18 from "../../assets/partner/p18.jpg";
+import partner19 from "../../assets/partner/p19.png";
+import partner20 from "../../assets/partner/p20.jpg";
 const Review = () => {
   return (
     <div>
       <section
         id="testimonies"
-        class="py-20 bg-gradient-to-r from-[#080C24] via-[#3342BF] to-[#080C24] via-[#00395D] to-[#080C24] text-gray-100"
+        class="py-20   bg-gradient-to-r from-[#24348f] via-[#3342BF] to-[#080C24] via-[#098bdc] to-[#24348f] text-gray-100"
       >
         <div class="transition duration-500 ease-in-out transform scale-100 translate-x-0 translate-y-0 opacity-100">
           <div class="mb-12 space-y-5 md:mb-16 md:text-center">
@@ -24,264 +43,79 @@ const Review = () => {
         <Marquee gradientColor={false} speed={80} gradient={false}>
           <div class=" ">
             {/* <Marquee   gradientColor={false} speed={80} gradient={false} play={true}> */}
-            <div class="flex  ">
-              <div
-                // style="box-shadow:0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)"
-                class="mx-5  bg-gradient-to-r from-[#155B59] via-[#080C24] to-[#0F1528]    flex flex-col max-w-md align-center justify-between border border-solid border-gray-200 dark:border-dark    rounded-xl"
-              >
-                <div class="flex flex-col px-6 pt-8  space-y-5">
-                  <svg
-                    width="24"
-                    height="18"
-                    viewBox="0 0 24 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="text-white dark:text-gray-600 fill-current"
-                  >
-                    <path
-                      d="M24 7.3h-5.1L22.3.4H17l-3.4 6.9v10.3H24V7.3zM10.3 17.6V7.3H5L8.6.4H3.4L0 7.3v10.3h10.3z"
-                      fill="current"
-                    ></path>
-                  </svg>
-                  <p
-                    class=" text-lg body-medium m-0 text-white dark:text-dark-contrastText"
-                    // style="hyphens:auto"
-                  >
-                    These are high-quality courses. Trust me. I own around 10
-                    and the price is worth it for the content quality.{" "}
-                    <a href="https://twitter.com/EducativeInc?ref_src=twsrc%5Etfw">
-                      @EducativeInc
-                    </a>{" "}
-                    came at the right time in my career. I'm understanding
-                    topics better than with any book or online video tutorial
-                    I've done. Truly made for developers. Thanks{" "}
-                    {/* <a href="https://t.co/EeKruv5hxM">
-                      https://t.co/EeKruv5hxM
-                    </a> */}
-                  </p>
-                </div>
-
-                <div class="flex justify-between items-center px-5 py-5">
-                  <div>
-                    <p class="font-display text-base text-white">
-                      Sheryl Berge
-                    </p>
-                  </div>
-                  <div class="overflow-hidden rounded-full ">
-                    <img
-                      alt=""
-                      class="h-14 w-14 object-cover bg-transparent"
-                      src="https://randomuser.me/api/portraits/men/15.jpg"
-                    />
-                  </div>
-                </div>
+            <div class="   ">
+              <div class="  m x-5    gap-5 flex     align-center justify-center    rounded-xl">
+                <img alt="" class="h-32 w-52  p-2 rounded  " src={partner4} />
+                <img alt="" class="h-32 w-52   p-2  rounded  " src={partner5} />
+                <img alt="" class="h-32 w-52   p-2  rounded  " src={partner6} />
+                <img alt="" class="h-32 w-52  p-2  rounded  " src={partner7} />
+                <img alt="" class="h-32 w-52  p-2   rounded  " src={partner8} />
+                <img alt="" class="h-32 w-52  p-2   rounded  " src={partner9} />
+                <img
+                  alt=""
+                  class="h-32 w-52   p-2  rounded  "
+                  src={partner10}
+                />
+                {/* <img alt="" class="h-32 w-52   rounded  " src={partner18} /> */}
+                <img
+                  alt=""
+                  class="h-32 w-52  p-2   rounded  "
+                  src={partner20}
+                />
               </div>
-              <div
-                // style="box-shadow:0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)"
-                class="mx-5  bg-gradient-to-r from-[#5E371A] via-[#080C24] to-[#1A0D18]    flex flex-col max-w-md align-center justify-between border border-solid border-gray-200 dark:border-dark    rounded-xl"
-              >
-                <div class="flex flex-col px-6 pt-8  space-y-5">
-                  <svg
-                    width="24"
-                    height="18"
-                    viewBox="0 0 24 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="text-white dark:text-gray-600 fill-current"
-                  >
-                    <path
-                      d="M24 7.3h-5.1L22.3.4H17l-3.4 6.9v10.3H24V7.3zM10.3 17.6V7.3H5L8.6.4H3.4L0 7.3v10.3h10.3z"
-                      fill="current"
-                    ></path>
-                  </svg>
-                  <p
-                    class=" text-lg body-medium m-0 text-white dark:text-dark-contrastText"
-                    // style="hyphens:auto"
-                  >
-                    These are high-quality courses. Trust me. I own around 10
-                    and the price is worth it for the content quality.{" "}
-                    <a href="https://twitter.com/EducativeInc?ref_src=twsrc%5Etfw">
-                      @EducativeInc
-                    </a>{" "}
-                    came at the right time in my career. I'm understanding
-                    topics better than with any book or online video tutorial
-                    I've done. Truly made for developers. Thanks{" "}
-                    {/* <a href="https://t.co/EeKruv5hxM">
-                      https://t.co/EeKruv5hxM
-                    </a> */}
-                  </p>
-                </div>
+            </div>
+          </div>
+        </Marquee>
+        <Marquee
+          gradientColor={false}
+          speed={80}
+          gradient={false}
+         
+        >
+          <div class=" ">
+            {/* <Marquee   gradientColor={false} speed={80} gradient={false} play={true}> */}
+            <div class="   ">
+              <div class="   py-5    gap-5 flex   max-w-md align-center justify-center    rounded-xl">
+                <img
+                  alt=""
+                  class="h-32 w-52   p-2   rounded  "
+                  src={partner2}
+                />
 
-                <div class="flex justify-between items-center px-5 py-5">
-                  <div>
-                    <p class="font-display text-base text-white">
-                      Sheryl Berge
-                    </p>
-                  </div>
-                  <div class="overflow-hidden rounded-full ">
-                    <img
-                      alt=""
-                      class="h-14 w-14 object-cover bg-transparent"
-                      src="https://randomuser.me/api/portraits/men/15.jpg"
-                    />
-                  </div>
-                </div>
+                <img
+                  alt=""
+                  class="h-32 w-52  p-2   rounded  "
+                  src={partner12}
+                />
+                <img
+                  alt=""
+                  class="h-32 w-52  p-2   rounded  "
+                  src={partner13}
+                />
+
+                <img
+                  alt=""
+                  class="h-32 w-52  p-2   rounded   "
+                  src={partner1}
+                />
+                <img
+                  alt=""
+                  class="h-32 w-52   p-2  rounded  "
+                  src={partner14}
+                />
+                <img
+                  alt=""
+                  class="h-32 w-52   p-2  rounded  "
+                  src={partner15}
+                />
+
+                <img
+                  alt=""
+                  class="h-32 w-52   p-2  rounded  "
+                  src={partner17}
+                />
+                <img alt="" class="h-32 w-52   p-2  rounded  " src={partner3} />
               </div>
-
-              <div
-                // style="box-shadow:0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)"
-                class="mx-5  bg-gradient-to-r from-[#1AA855] via-[#051431] to-[#013125]    flex flex-col max-w-md align-center justify-between border border-solid border-gray-200 dark:border-dark    rounded-xl"
-              >
-                <div class="flex flex-col px-6 pt-8  space-y-5">
-                  <svg
-                    width="24"
-                    height="18"
-                    viewBox="0 0 24 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="text-white dark:text-gray-600 fill-current"
-                  >
-                    <path
-                      d="M24 7.3h-5.1L22.3.4H17l-3.4 6.9v10.3H24V7.3zM10.3 17.6V7.3H5L8.6.4H3.4L0 7.3v10.3h10.3z"
-                      fill="current"
-                    ></path>
-                  </svg>
-                  <p
-                    class=" text-lg body-medium m-0 text-white dark:text-dark-contrastText"
-                    // style="hyphens:auto"
-                  >
-                    These are high-quality courses. Trust me. I own around 10
-                    and the price is worth it for the content quality.{" "}
-                    <a href="https://twitter.com/EducativeInc?ref_src=twsrc%5Etfw">
-                      @EducativeInc
-                    </a>{" "}
-                    came at the right time in my career. I'm understanding
-                    topics better than with any book or online video tutorial
-                    I've done. Truly made for developers. Thanks{" "}
-                    {/* <a href="https://t.co/EeKruv5hxM">
-                      https://t.co/EeKruv5hxM
-                    </a> */}
-                  </p>
-                </div>
-
-                <div class="flex justify-between items-center px-5 py-5">
-                  <div>
-                    <p class="font-display text-base text-white">
-                      Sheryl Berge
-                    </p>
-                  </div>
-                  <div class="overflow-hidden rounded-full ">
-                    <img
-                      alt=""
-                      class="h-14 w-14 object-cover bg-transparent"
-                      src="https://randomuser.me/api/portraits/men/15.jpg"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div
-                // style="box-shadow:0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)"
-                class="mx-5  bg-gradient-to-r from-[#680C68] via-[#080C24] to-[#150620]    flex flex-col max-w-md align-center justify-between border border-solid border-gray-200 dark:border-dark    rounded-xl"
-              >
-                <div class="flex flex-col px-6 pt-8  space-y-5">
-                  <svg
-                    width="24"
-                    height="18"
-                    viewBox="0 0 24 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="text-white dark:text-gray-600 fill-current"
-                  >
-                    <path
-                      d="M24 7.3h-5.1L22.3.4H17l-3.4 6.9v10.3H24V7.3zM10.3 17.6V7.3H5L8.6.4H3.4L0 7.3v10.3h10.3z"
-                      fill="current"
-                    ></path>
-                  </svg>
-                  <p
-                    class=" text-lg body-medium m-0 text-white dark:text-dark-contrastText"
-                    // style="hyphens:auto"
-                  >
-                    These are high-quality courses. Trust me. I own around 10
-                    and the price is worth it for the content quality.{" "}
-                    <a href="https://twitter.com/EducativeInc?ref_src=twsrc%5Etfw">
-                      @EducativeInc
-                    </a>{" "}
-                    came at the right time in my career. I'm understanding
-                    topics better than with any book or online video tutorial
-                    I've done. Truly made for developers. Thanks{" "}
-                    {/* <a href="https://t.co/EeKruv5hxM">
-                      https://t.co/EeKruv5hxM
-                    </a> */}
-                  </p>
-                </div>
-
-                <div class="flex justify-between items-center px-5 py-5">
-                  <div>
-                    <p class="font-display text-base text-white">
-                      Sheryl Berge
-                    </p>
-                  </div>
-                  <div class="overflow-hidden rounded-full ">
-                    <img
-                      alt=""
-                      class="h-14 w-14 object-cover bg-transparent"
-                      src="https://randomuser.me/api/portraits/men/15.jpg"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div
-                // style="box-shadow:0px 2px 4px rgba(0, 0, 0, 0.06), 0px 4px 6px rgba(0, 0, 0, 0.1)"
-                class="mx-5  bg-gradient-to-r from-[#9d2626] via-[#051431] to-[#610303]    flex flex-col max-w-md align-center justify-between border border-solid border-gray-200 dark:border-dark    rounded-xl"
-              >
-                <div class="flex flex-col px-6 pt-8  space-y-5">
-                  <svg
-                    width="24"
-                    height="18"
-                    viewBox="0 0 24 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="text-white dark:text-gray-600 fill-current"
-                  >
-                    <path
-                      d="M24 7.3h-5.1L22.3.4H17l-3.4 6.9v10.3H24V7.3zM10.3 17.6V7.3H5L8.6.4H3.4L0 7.3v10.3h10.3z"
-                      fill="current"
-                    ></path>
-                  </svg>
-                  <p
-                    class=" text-lg body-medium m-0 text-white dark:text-dark-contrastText"
-                    // style="hyphens:auto"
-                  >
-                    These are high-quality courses. Trust me. I own around 10
-                    and the price is worth it for the content quality.{" "}
-                    <a href="https://twitter.com/EducativeInc?ref_src=twsrc%5Etfw">
-                      @EducativeInc
-                    </a>{" "}
-                    came at the right time in my career. I'm understanding
-                    topics better than with any book or online video tutorial
-                    I've done. Truly made for developers. Thanks{" "}
-                    {/* <a href="https://t.co/EeKruv5hxM">
-                      https://t.co/EeKruv5hxM
-                    </a> */}
-                  </p>
-                </div>
-
-                <div class="flex justify-between items-center px-5 py-5">
-                  <div>
-                    <p class="font-display text-base text-white">
-                      Sheryl Berge
-                    </p>
-                  </div>
-                  <div class="overflow-hidden rounded-full ">
-                    <img
-                      alt=""
-                      class="h-14 w-14 object-cover bg-transparent"
-                      src="https://randomuser.me/api/portraits/men/15.jpg"
-                    />
-                  </div>
-                </div>
-              </div>
-              
             </div>
           </div>
         </Marquee>
