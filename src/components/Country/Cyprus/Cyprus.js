86f87caebceba25c24f75488dedfcbc6ec845767
Sadnan-2019 <sadnan.wow@gmail.com>
2024-05-29 17:11:56 +0600

    cyprus

--- src/components/Country/Cyprus/Cyprus.js
+++ src/components/Country/Cyprus/Cyprus.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CyprusLanding from './CyprusLanding';
+import WhyCyprus from './WhyCyprus';
 
 const Cyprus = () => {
     return (
         <div>
             <CyprusLanding></CyprusLanding>
+            <WhyCyprus></WhyCyprus>
         </div>
     );
 };
---
+++ src/components/Country/Cyprus/WhyCyprus.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const WhyCyprus = () => {
+    return (
+        <div>
+            <div class="relative p-4">
+        <div class="max-w-3xl mx-auto">
+          <div class="  bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+            <div class="">
+              <h1 href="#" class="text-gray-900 italic font-bold text-4xl">
+                Study in Cyprus
+              </h1>
+              <h1 href="#" class="text-gray-900 italic mt-2 font-bold text-2xl">
+              Cyprus has become a popular destination among international
+                students for several reasons
+              </h1>
+              <div class="py-5 text-sm font-regular text-gray-900 flex"></div>
+              <hr />
+
+              <h3 class="text-2xl font-bold my-5">
+                1.High-Ranked Universities
+              </h3>
+              <p class="text-base leading-8 my-5">
+              Cyprus is home to prestigious universities, with six of them
+                ranking in the top 500 globally according to the QS World
+                University Rankings 2024.
+              </p>
+              {/* <blockquote class="text-md italic leading-8 my-5 p-5 text-indigo-600 font-semibold">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </blockquote> */}
+
+              <h3 class="text-2xl font-bold my-5  ">
+                2.Post-Study Work Rights
+              </h3>
+              <p class="text-base leading-8 my-5  ">
+                After completing their studies, students can benefit from up to
+                2 years of post-study work rights in Cyprus.
+              </p>
+              <h3 class="text-2xl font-bold my-5">3.Best Student City</h3>
+              <p class="text-base leading-8 my-5">
+              Nicosia, one of Cyprus major cities, is ranked as one of the
+                best student cities in the world.
+              </p>
+              <h3 class="text-2xl font-bold my-5">4.MNC Headquarters</h3>
+              <p class="text-base leading-8 my-5">
+                 Many Fortune 500 companies have their
+                headquarters in Cyprus, providing job opportunities.
+              </p>
+              <h3 class="text-2xl font-bold my-5">5.Scholarships</h3>
+              <p class="text-base leading-8 my-5">
+               Cyprus offers attractive scholarships for eligible international students.
+              </p>
+               
+              <h3 class="text-2xl font-bold my-5">5.Cost of Studying and Living</h3>
+              <p class="text-base leading-8 my-5">
+              The cost of studying in Cyprus ranges from approximately €6,000 to €14,000.
+              </p>
+              <p class="text-base leading-8 my-5">
+              Living expenses may amount to around €3,000 to €5,000.
+              </p>
+            
+              
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
+    );
+};
+
+export default WhyCyprus;
