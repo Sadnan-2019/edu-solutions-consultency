bf44c5e13064c2545cc83a5c7e3a691d84bbee63
Sadnan-2019 <sadnan.wow@gmail.com>
2024-06-06 14:57:13 +0600

    inquery  r

--- src/components/Inquery/Inquery.js
+++ src/components/Inquery/Inquery.js
@@ -11,10 +11,10 @@ const Inquery = () => {
 
     emailjs
       .sendForm(
-        "service_g4ete88",
-        "template_d9ss8cg",
+        "service_nncslnk",
+        "template_eg4gcvz",
         form.current,
-        "Ha_sV1MspGoTm0fU2"
+        "qdrGuamO3WZnWY8i0"
       )
       .then(
         (result) => {
@@ -30,97 +30,95 @@ const Inquery = () => {
     <div className=" bg-gradient-to-r from-[#24348f] via-[#3342BF] to-[#080C24] via-[#098bdc] to-[#24348f]">
       <div className="grid sm:grid-cols-1 lg:grid-cols-2   items-center justify-items-center px-8">
         <div>
-        
-            <div
-              class="flex flex-col items-start justify-start p-8     rounded-xl relative z-10"
-            >
-              <p class="w-full text-4xl fade-up font-medium text-center text-white leading-snug font-serif">
-                Free Consultation
-              </p>
-              <form  onSubmit={sendEmail} ref={form}>
-                <div class="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
-                  <div data-aos="fade-up" class="relative">
-                    <p
-                      class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100
-      absolute"
-                    >
-                      Name
-                    </p>
-                    <input
-                      placeholder="John"
-                      type="text"
-                      class="border  placeholder-gray-400 focus:outline-none
+          <div class="flex flex-col  justify-center p-8     rounded-xl relative z-10">
+            <p class="w-full text-4xl fade-up font-medium text-center text-white leading-snug font-serif">
+              Free Consultation
+            </p>
+            <form onSubmit={sendEmail} ref={form}>
+              <div class="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
+                <div data-aos="fade-up" class="relative">
+                  <p class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100 absolute">
+                    Name
+                  </p>
+                  <input
+                  name="name"
+                    placeholder="John"
+                    type="text"
+                    class="border  placeholder-gray-400 focus:outline-none
       focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
       border-gray-300 rounded-md"
-                    />
-                  </div>
-                  <div data-aos="fade-up" class="relative">
-                    <p class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100 absolute">
-                      Email
-                    </p>
-                    <input
-                      placeholder="123@ex.com"
-                      type="text"
-                      class="border placeholder-gray-400 focus:outline-none
-      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-      border-gray-300 rounded-md"
-                    />
-                  </div>
-                  <div data-aos="fade-up" class="relative">
-                    <p class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100 absolute">
-                      Phone Number
-                    </p>
-                    <input
-                      placeholder="Phone Number"
-                      type="text"
-                      class="border placeholder-gray-400 focus:outline-none
-      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-      border-gray-300 rounded-md"
-                    />
-                  </div>
-                  <div data-aos="fade-up" class="relative">
-                    <p
-                      class=" pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100
-      absolute"
-                    >
-                      Service Name
-                    </p>
-                    <input
-                      placeholder="Service Name"
-                      type="text"
-                      class="border placeholder-gray-400 focus:outline-none
-      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-      border-gray-300 rounded-md"
-                    />
-                  </div>
-                  <div data-aos="fade-up" class="relative">
-                    <p
-                      class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100
-      absolute"
-                    >
-                      Message
-                    </p>
-                    <textarea
-                      placeholder="Message"
-                      type="text"
-                      class="border placeholder-gray-400 focus:outline-none
-      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-      border-gray-300 rounded-md"
-                    />
-                  </div>
-
-                  <div className="">
-                    <input
-                      type="submit"
-                      className="btn bg-gradient-to-r from-[#080C24] via-[#3342BF] to-[#080C24]  w-full max-w-x "
-                      value="Send message"
-                      style={{ backgroundColor: "#453364", color: "white" }}
-                    />
-                  </div>
+                  />
                 </div>
-              </form>
-            </div>
-      
+                <div data-aos="fade-up" class="relative">
+                  <p class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100 absolute">
+                    Email
+                  </p>
+                  <input
+                  name="email"
+                    placeholder="123@ex.com"
+                    type="email"
+                    class="border placeholder-gray-400 focus:outline-none
+      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+      border-gray-300 rounded-md"
+                  />
+                </div>
+                <div data-aos="fade-up" class="relative">
+                  <p class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100 absolute">
+                    Phone Number
+                  </p>
+                  <input
+                  name="mobile"
+                    placeholder="Phone Number"
+                    type="text"
+                    class="border placeholder-gray-400 focus:outline-none
+      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+      border-gray-300 rounded-md"
+                  />
+                </div>
+                <div data-aos="fade-up" class="relative">
+                  <p
+                    class=" pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100
+      absolute"
+                  >
+                    Service Name
+                  </p>
+                  <input
+                  name="service"
+                    placeholder="Service Name"
+                    type="text"
+                    class="border placeholder-gray-400 focus:outline-none
+      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+      border-gray-300 rounded-md"
+                  />
+                </div>
+                <div data-aos="fade-up" class="relative">
+                  <p
+                    class="  pt-0 pr-2 pb-0 pl-2 -mt-8 mr-0 mb-0 ml-2 font-medium text-gray-100
+      absolute"
+                  >
+                    Message
+                  </p>
+                  <textarea
+                  name="message"
+                    placeholder="Message"
+                    type="text"
+                    class="border placeholder-gray-400 focus:outline-none
+      focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
+      border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="">
+                  <input
+                    type="submit"
+                    className="btn bg-gradient-to-r from-[#080C24] via-[#3342BF] to-[#080C24]  w-full max-w-x "
+                    value="Send message"
+                    style={{ backgroundColor: "#453364", color: "white" }}
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
 
         <div className="slide-content sm:max-w-xs lg:max-w-xl text-left md:ml-8 lg:order-1 order-2   lg:mt-0 ">
